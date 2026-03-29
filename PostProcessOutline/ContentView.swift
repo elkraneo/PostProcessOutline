dbfa46d24d548f88a5a6d16af35cfd1b5324d739
Cristian Díaz <cristian@210x7.io>
2026-03-29 20:42:54 +0200

    Initial Commit

---
+++ PostProcessOutline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PostProcessOutline
+//
+//  Created by Cristian Díaz on 29.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PostProcessOutline/PostProcessOutlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostProcessOutlineApp.swift
+//  PostProcessOutline
+//
+//  Created by Cristian Díaz on 29.03.26.
+//
+
+import SwiftUI
+
+@main
+struct PostProcessOutlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
